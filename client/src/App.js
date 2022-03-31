bd79ae78cec58c597be7a55e9f25d14a9b011c1e
unknown <okwuosahpaschal@gmail.com>
2022-03-31 23:21:19 +0100

    app.js modify commit

--- client/src/App.js
+++ client/src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
 
   handleInputChange = (event) => {
     const target = event.target;
-    //const value = target.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
     this.setState({
       [name]: value
