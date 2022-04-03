95a6c490d447fb1f4be194b816da5bef9dccba23
Paschal <58183764+paschal533@users.noreply.github.com>
2022-04-03 20:26:07 +0100

    Update myTokenSale

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -12,7 +12,7 @@ contract("TokenSale Test", async (accounts) => {
 
     const [deployerAccount, recipient, anotherAccount] = accounts;
 
-    it('should not have any tokens in my deployer', async () => {
+    it('should not have any tokens in my deployerAccount', async () => {
         let instance = await Token.deployed();
         return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
     });
