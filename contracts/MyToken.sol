b36c42959e13b7fdb6121aebac5cad0601f6dc4d
Paschal <58183764+paschal533@users.noreply.github.com>
2022-05-24 22:41:42 +0100

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.6.1;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+//import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract MyToken is ERC20, ERC20Detailed {
     constructor(uint256 initialSupply) ERC20Detailed("StarDucks Cappucino token", "CAPPU", 0) public {
