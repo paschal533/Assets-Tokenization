5fb5356e207a1caac40a06fe313d5becdb7423b2
Paschal <58183764+paschal533@users.noreply.github.com>
2022-05-24 22:42:35 +0100

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.6.1;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract MyToken is ERC20, ERC20Detailed {
