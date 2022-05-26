0cad6e6e62cff0bf5ad1480902f1e5563984fd51
Paschal <58183764+paschal533@users.noreply.github.com>
2022-05-26 21:54:21 +0100

    Update CrowdSale

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/GSN/Context.sol";
-//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
