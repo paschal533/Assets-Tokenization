c038f6d17713cd89bf4b9751688d6a4138ccc2f0
Paschal <58183764+paschal533@users.noreply.github.com>
2022-05-27 22:35:17 +0100

    Update KycContract

--- contracts/KycContract.sol
+++ contracts/KycContract.sol
@@ -4,7 +4,7 @@ pragma solidity 0.6.1;
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract KycContract is Ownable {
-    mapping(address => bool) allowed;
+    //mapping(address => bool) allowed;
 
     function setKycCompleted(address _addr) public onlyOwner {
         allowed[_addr] = true;
