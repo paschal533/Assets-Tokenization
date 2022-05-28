f520b2aa997e39ee1200c1e66641d0625b0770ca
Paschal <58183764+paschal533@users.noreply.github.com>
2022-05-28 21:22:28 +0100

    Update CrowdSale

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/GSN/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+//import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title Crowdsale
